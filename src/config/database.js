fb666ebb6210cd5611c1256ef4bba748815c69d2
Lucas <lucashen@gmail.com>
2020-10-08 07:32:57 -0300

    config the database

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+	host: "127.0.0.1",
+	username: "root",
+	password: null,
+	database: "database_test",
+	dialect: "postgres",
+	operatorsAliases: false,
+	logging: false,
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true
+	}
+};
